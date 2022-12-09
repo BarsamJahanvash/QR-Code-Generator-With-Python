1de076024d3d9ed251e4148d93df25015f63f5ca
BarsamJahanvash <108729583+BarsamJahanvash@users.noreply.github.com>
2022-12-09 19:35:56 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# Libraries
+from customtkinter import *
+import qrcode as qr
+from PIL import Image
+from tkinter import PhotoImage
+import os
+#  QR Code Generator Function
+def Generate():
+    URL = LinkEntry.get()
+    qrcodeimg = qr.make(URL)
+    qrcodeimg.save("Generated.png") 
+    FinalImage = CTkImage(dark_image=Image.open("Generated.png"),size=(200,200))
+    QrCode.configure(image=FinalImage)
+    os.remove("Generated.png")
+# GUI
+primarycolor = "#30323D"
+secodarycolor = "#4D5061"
+win = CTk() # Make A Window
+win.title("QR Code Generator") # Set A Title For Window
+win.resizable(0,0)
+win.geometry("400x600") # Window Size
+win.config(bg = primarycolor) #Change Window Background To Primarycolor
+Header = CTkLabel(win,text="QR Code Generator",font=("Roboto Slab",40),fg_color=primarycolor)
+Header.place(relx=0.5,rely=0.1,anchor=CENTER)
+MainFrame = CTkFrame(win,bg_color= primarycolor,border_color=primarycolor,fg_color=secodarycolor,corner_radius=20,border_width=2,height=450,width=300)
+MainFrame.place(relx=0.5,rely=0.55,anchor=CENTER)
+EntryLabel = CTkLabel(win,text="Enter Your Website Url :",fg_color=secodarycolor,bg_color=secodarycolor,text_color="White",font=("Robot Slab",18))
+EntryLabel.place(relx=0.5,rely=0.25,anchor=CENTER)
+LinkEntry = CTkEntry(win,fg_color=primarycolor,bg_color=secodarycolor,width=250,placeholder_text="Enter You Website Url :",placeholder_text_color="White")
+LinkEntry.place(relx = 0.5,rely = 0.31,anchor=CENTER)
+GenerateButton = CTkButton(win,text="Generate",fg_color="#cdd1c4",text_color=primarycolor,hover_color="#cdd1c4",bg_color=secodarycolor,command=Generate)
+GenerateButton.place(relx=0.5,rely=0.38,anchor=CENTER)
+QrCode = CTkLabel(win,text=None,bg_color=secodarycolor,fg_color=secodarycolor)
+QrCode.place(relx=0.5,rely=0.6,anchor = CENTER)
+win.mainloop()
---
+++ test.html
@@ -0,0 +1,6 @@
+<h1 aligh="center">HI😊</h1>
+<h3 align="center">
+    This project is a small and medium project with Python programming language.
+    This project is a QR code generator.
+    I made a beautiful and modern graphic environment for this project using the CustomTkinter library.
+</h3>
